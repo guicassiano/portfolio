8d63c384a9c2ceb50aa95dc0d0b431293e11781f
Guilherme <guicassiano8@gmail.com>
2024-02-02 23:19:46 -0300

    modifying the project structure

--- assets/js/menu.js
+++
@@ -1,6 +0,0 @@
-const menu = document.getElementById("menu")
-const menuMobile = document.getElementById("menuMobile")
-
-menu.addEventListener("click", ()=>{
-    menuMobile.classList.toggle("menu-mobile")
-})
