c535347d7ccbd8fce7095f6ca6058420572cf68e
Guilherme <guicassiano8@gmail.com>
2024-03-04 11:10:24 -0300

    Adicionando projeto fetch-api

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,14 +1,14 @@
-function cardProject(nomeProject,id,...icons){
+function cardProject(nomeProject,id,idBtn,...icons){
     return `    
-<div class="card-project">
+<div class="card-project" id="${id}">
     <div class="card-body">
-        <p>${nomeProject}</p>
-        <div class="icon-skill">
-            ${icons}
-        </div>
+    <div class="icon-skill">
+    ${icons}
+    </div>
     </div>
     <div class="card-footer">
-        <button id="${id}" class="btn-more">ver mais</button>
+    <p>${nomeProject}</p>
+        <button id="${idBtn}" class="btn-more">ver mais</button>
     </div>
 </div>
     `
@@ -16,11 +16,12 @@ function cardProject(nomeProject,id,...icons){
 
 document.querySelector("#projects").innerHTML = `
 <h2>Projetos</h2>
-${cardProject("nlw copa","btnNlwCopa","<img src='./assets/img/html-5.png' alt='icone html'>","<img src='./assets/img/css-3.png' alt='icone css'>","<img src='./assets/img/script-java.png' alt='icone js'>")}
-${cardProject("dark mode","btnDarkMode","<img src='./assets/img/html-5.png' alt=''>","<img src='./assets/img/css-3.png' alt='icone css'>","<img src='./assets/img/script-java.png' alt='icone js'>")}
-${cardProject("calculadora","btnCalculadora","<img src='./assets/img/html-5.png' alt=''>","<img src='./assets/img/css-3.png' alt='icone css'>","<img src='./assets/img/script-java.png' alt='icone js'>")}
-${cardProject("cronômetro","btnCronometro","<img src='./assets/img/html-5.png' alt=''>","<img src='./assets/img/css-3.png' alt='icone css'>","<img src='./assets/img/script-java.png' alt='icone js'>")}
-${cardProject("relogio digital","btnRelogioDigital","<img src='./assets/img/html-5.png' alt=''>","<img src='./assets/img/css-3.png' alt='icone css'>","<img src='./assets/img/script-java.png' alt='icone js'>")}
+${cardProject("fetch-api","cardFetchApi","btnFetchApi","<img src='./assets/img/html-5.png' alt='icone html'>","<img src='./assets/img/css-3.png' alt='icone css'>","<img src='./assets/img/script-java.png' alt='icone js'>")}
+${cardProject("nlw copa","cardNlwCopa","btnNlwCopa","<img src='./assets/img/html-5.png' alt='icone html'>","<img src='./assets/img/css-3.png' alt='icone css'>","<img src='./assets/img/script-java.png' alt='icone js'>")}
+${cardProject("dark mode","cardDarkMode","btnDarkMode","<img src='./assets/img/html-5.png' alt=''>","<img src='./assets/img/css-3.png' alt='icone css'>","<img src='./assets/img/script-java.png' alt='icone js'>")}
+${cardProject("calculadora","cardCalculadora","btnCalculadora","<img src='./assets/img/html-5.png' alt=''>","<img src='./assets/img/css-3.png' alt='icone css'>","<img src='./assets/img/script-java.png' alt='icone js'>")}
+${cardProject("cronômetro","cardCronometro","btnCronometro","<img src='./assets/img/html-5.png' alt=''>","<img src='./assets/img/css-3.png' alt='icone css'>","<img src='./assets/img/script-java.png' alt='icone js'>")}
+${cardProject("relogio digital","cardRelogioDigital","btnRelogioDigital","<img src='./assets/img/html-5.png' alt=''>","<img src='./assets/img/css-3.png' alt='icone css'>","<img src='./assets/img/script-java.png' alt='icone js'>")}
 `
 
 function contentModal(imgProject,titleProject,paragraphProject,repositorio){
@@ -44,6 +45,11 @@ function contentModal(imgProject,titleProject,paragraphProject,repositorio){
     `
 }
 
+const modalFetchApi = document.createElement("div")
+modalFetchApi.setAttribute("id", "modalFetchApi")
+modalFetchApi.setAttribute("class", "modal hide")
+modalFetchApi.innerHTML = contentModal("<img src='./assets/img/fetch-api.png' alt='imagem do projeto'></img>","Fetch-api", "Blog com layout simples com foco em integrar uma API utilizando o Fetch API, onde é pego os títulos, paragrafos e comentários de cada post.","https://github.com/guicassiano/fetch-api.git")
+
 const modalNlwCopa = document.createElement("div")
 modalNlwCopa.setAttribute("id", "modalNlwCopa")
 modalNlwCopa.setAttribute("class", "modal hide")
@@ -52,28 +58,29 @@ modalNlwCopa.innerHTML = contentModal("<img src='./assets/img/nlw-copa.jpg' alt=
 const modalDarkMode = document.createElement("div")
 modalDarkMode.setAttribute("id", "modalDarkMode")
 modalDarkMode.setAttribute("class", "modal hide")
-modalDarkMode.innerHTML = contentModal("","Dark Mode","Esse projeto tem como funcionalidade demonstrar o uso do modo escuro em uma página e salvar as preferências de tema escuro ou claro utilizando LocalStorage.","https://github.com/guicassiano/dark-mode.git")
+modalDarkMode.innerHTML = contentModal("<img src='./assets/img/dark-mode.png' alt='imagem do projeto'></img>","Dark Mode","Esse projeto tem como funcionalidade demonstrar o uso do modo escuro em uma página e salvar as preferências de tema escuro ou claro utilizando LocalStorage.","https://github.com/guicassiano/dark-mode.git")
 
 const modalCalculadora = document.createElement("div")
 modalCalculadora.setAttribute("id", "modalCalculadora")
 modalCalculadora.setAttribute("class", "modal hide")
-modalCalculadora.innerHTML = contentModal("","Calculadora JS","Calculadora criada utilizando JavaScript POO, com o objetivo de aprimorar a habilidade em JavaScript POO.","https://github.com/guicassiano/calculadora.git")
+modalCalculadora.innerHTML = contentModal("<img src='./assets/img/calculadora.png' alt='imagem do projeto'></img>","Calculadora JS","Calculadora criada utilizando JavaScript POO, com o objetivo de aprimorar a habilidade em JavaScript POO.","https://github.com/guicassiano/calculadora.git")
 
 const modalCronometro = document.createElement("div")
 modalCronometro.setAttribute("id", "modalCronometro")
 modalCronometro.setAttribute("class", "modal hide")
-modalCronometro.innerHTML = contentModal("","Cronômetro JS","Cronômetro criado utilizando a linguagem JavaScript, com o objetivo de aprimorar a habilidade em manipulação de data e hora.","https://github.com/guicassiano/cronometro.git")
+modalCronometro.innerHTML = contentModal("<img src='./assets/img/cronometro.png' alt='imagem do projeto'></img>","Cronômetro JS","Cronômetro criado utilizando a linguagem JavaScript, com o objetivo de aprimorar a habilidade em manipulação de data e hora.","https://github.com/guicassiano/cronometro.git")
 
 const modalRelogioDigital = document.createElement("div")
 modalRelogioDigital.setAttribute("id", "modalRelogioDigital")
 modalRelogioDigital.setAttribute("class", "modal hide")
-modalRelogioDigital.innerHTML = contentModal("","Relógio Digital","Relógio Digital criado utilizando a linguagem JavaScript, com o objetivo de aprimorar a habilidade em manipulação de data e hora.","https://github.com/guicassiano/relogio-digital.git")
+modalRelogioDigital.innerHTML = contentModal("<img src='./assets/img/relogio-digital.png' alt='imagem do projeto'></img>","Relógio Digital","Relógio Digital criado utilizando a linguagem JavaScript, com o objetivo de aprimorar a habilidade em manipulação de data e hora.","https://github.com/guicassiano/relogio-digital.git")
 
 document.body.appendChild(modalNlwCopa)
 document.body.appendChild(modalDarkMode)
 document.body.appendChild(modalCalculadora)
 document.body.appendChild(modalCronometro)
 document.body.appendChild(modalRelogioDigital)
+document.body.appendChild(modalFetchApi)
 
 const fade = document.getElementById("fade")
 const modal1 = document.querySelector("#modalNlwCopa")
@@ -81,13 +88,19 @@ const modal2 = document.querySelector("#modalDarkMode")
 const modal3 = document.querySelector("#modalCalculadora")
 const modal4 = document.querySelector("#modalCronometro")
 const modal5 = document.querySelector("#modalRelogioDigital")
+const modal6 = document.querySelector("#modalFetchApi")
 const openNlwCopa = document.querySelector("#btnNlwCopa")
 const openDarkMode = document.querySelector("#btnDarkMode")
 const openCalculadora = document.querySelector("#btnCalculadora")
 const openCronometro = document.querySelector("#btnCronometro")
 const openRelogioDigital = document.querySelector("#btnRelogioDigital")
+const openFetchApi = document.querySelector("#btnFetchApi")
 const btnCloseModal = [...document.querySelectorAll(".close-modal")]
 
+openFetchApi.addEventListener("click", (evt)=>{
+        fade.classList.toggle("hide")
+        modal6.classList.toggle("hide")
+})
 openNlwCopa.addEventListener("click", (evt)=>{
         fade.classList.toggle("hide")
         modal1.classList.toggle("hide")
@@ -120,6 +133,7 @@ btnCloseModal.map((el)=>{
     modal3.classList.add("hide")
     modal4.classList.add("hide")
     modal5.classList.add("hide")
+    modal6.classList.add("hide")
     fade.classList.add("hide")
 })  
 })
